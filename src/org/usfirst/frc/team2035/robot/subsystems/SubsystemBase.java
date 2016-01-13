4e6a1cad7484ae29c2701ad2a70763b46d0c047e
Abby Lambert <absterninja@gmail.com>
2016-01-12 19:14:48 -0800

    Added subsystem

---
+++ src/org/usfirst/frc/team2035/robot/subsystems/SubsystemBase.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team2035.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public abstract class SubsystemBase extends Subsystem {
+	
+	private String name;
+	
+	public SubsystemBase()
+	{
+		super();
+		name = "";
+	}
+	
+	public SubsystemBase(String n)
+	{
+		super();
+		name = n;
+	}
+	
+	public abstract void init();
+	
+	public abstract void end();
+
+
+}
