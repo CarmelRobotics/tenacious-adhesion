ea501cef77d54063ec79f336f48939e017743822
Henry Kou <kouhenry@yahoo.com>
2016-01-26 15:44:17 -0800

    lol changed 15 second auto drive forward to 1 second

--- src/org/usfirst/frc/team2035/robot/commands/AutoDriveForward.java
+++ src/org/usfirst/frc/team2035/robot/commands/AutoDriveForward.java
@@ -28,7 +28,7 @@ public class AutoDriveForward extends CommandBase
 	protected void execute()
 	{
 		 theDriveTrain.drive(RobotMap.AutonomousSpeed);
-	        if (time.get() > 15.00) 
+	        if (time.get() > 1.00) 
 	        {
 	            timesup = true;
 	        }
